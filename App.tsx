aeef183f318d49a63d8381db6855a628c3f1cf82
Jonna Bohol BSCPE 1-G DAY PROGRAM <159886481+Boholjonna@users.noreply.github.com>
2026-04-24 03:10:03 +0800

    Wire ToastProvider into App component

--- App.tsx
+++ App.tsx
@@ -9,14 +9,17 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from './src/contexts/ThemeContext';
 import { DataProvider } from './src/contexts/DataContext';
+import { ToastProvider } from './src/utils/Toast';
 import { AppNavigator } from './src/navigation';
 
 export default function App() {
   return (
     <ThemeProvider>
       <DataProvider>
-        <AppNavigator />
-        <StatusBar style="light" />
+        <ToastProvider>
+          <AppNavigator />
+          <StatusBar style="light" />
+        </ToastProvider>
       </DataProvider>
     </ThemeProvider>
   );
